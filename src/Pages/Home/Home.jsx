d18fcb9199919702be881de49f70154f71a7ad6e
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2025-01-17 18:36:13 -0600

    colors

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,9 +3,9 @@ import { PacmanLoader } from "react-spinners";
 
 const Home = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold text-gray-800 mb-4">Hola!!</h1>
-            <p className="text-lg text-gray-600">Esta página está en construcción. ¡Vuelve pronto!</p>
+        <div className="flex flex-col items-center justify-center min-h-screen  bg-slate-950">
+            <h1 className="text-4xl font-bold text-white mb-4">Hola!!</h1>
+            <p className="text-lg text-gray-50">Esta página está en construcción. ¡Vuelve pronto!</p>
             <PacmanLoader className="m-4" color="yellow" />
         </div>
     );
