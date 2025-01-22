50a127a037c7b5da4b8f0691667a3343c8236977
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2025-01-22 08:54:32 -0600

    fix: ui car erros

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,25 +1,26 @@
 const ProjectCard = ({ title, sortDescription, description, tags }) => {
   return (
-    <>
-      <div className="border border-white md:h-36 rounded-md p-3 w-64 sm:w-full sm:h-32 hover:bg-slate-800 duration-500 transition">
-        <h5 className="text-white text-xl sm:text-sm sm:font-semibold">
-          {title}
-        </h5>
-        <p className="sm:block md:hidden text-white text-sm hidden italic">
-          {sortDescription}
-        </p>
-        <p className="text-white text-sm italic  sm:hidden md:block ">
-          {description}
-        </p>
-        <ul className="flex flex-wrap gap-2 m-2">
-          {tags.map((tag, index) => (
-            <li key={index} className="text-white border rounded-sm p-0.5 text-sm italic bg-slate-600 opacity-50">
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <div className="border border-white md:h-36 rounded-md p-3 w-full max-w-sm sm:w-full sm:h-32 hover:bg-slate-800 duration-500 transition box-content">
+      <h5 className="text-white text-xl sm:text-sm sm:font-semibold">
+        {title}
+      </h5>
+      <p className="sm:block md:hidden text-white text-sm hidden italic">
+        {sortDescription}
+      </p>
+      <p className="text-white text-sm italic sm:hidden md:block">
+        {description}
+      </p>
+      <ul className="flex flex-wrap gap-2 m-2">
+        {tags.map((tag, index) => (
+          <li
+            key={index}
+            className="text-white border rounded-sm p-0.5 text-sm italic bg-slate-600 opacity-50 max-w-full truncate"
+          >
+            {tag}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
