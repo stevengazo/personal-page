c1963c5d6f815efdc5324680a27cd23c5966faf1
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2025-01-22 14:16:00 -0600

    feat: cards

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,6 +1,11 @@
 import ContactCard from "../Components/ContactCard";
-import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
-
+import {
+  FaWhatsapp,
+  FaMailBulk,
+  FaLinkedin,
+  FaGithub,
+  FaTwitter,
+} from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -10,8 +15,32 @@ const Hero = () => {
         {/* White text for the heading with 3xl size */}
         <h2 className="text-white  absolute top-6 text-3xl">Contact</h2>
         <div>
-          <ul>
-            <ContactCard title={"Linkedin"} _icon={FaLinkedin} url={"https://www.linkedin.com/in/stevengazo/"} username={"stevengazo"} />
+          <ul className="flex md:flex-row sm:flex-col gap-4">
+            <ContactCard
+              title={"Whatsapp"}
+              _icon={FaWhatsapp}
+              url={"https://wa.me/86279806"}
+              username={"stevengazo"}
+            />
+
+            <ContactCard
+              title={"Linkedin"}
+              _icon={FaLinkedin}
+              url={"https://www.linkedin.com/in/stevengazo/"}
+              username={"stevengazo"}
+            />
+            <ContactCard
+              title={"Email"}
+              _icon={FaMailBulk}
+              url={"mailto:steven.gazo@hotmail.com"}
+              username={"stevengazo.gazo@hotmail.com"}
+            />
+            <ContactCard
+              title={"GitHub"}
+              _icon={FaGithub}
+              url={"https://github.com/stevengazo"}
+              username={"stevengazo"}
+            />
           </ul>
         </div>
       </div>
