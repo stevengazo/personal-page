449e14d57ba09b90d89dcd924107ed92e91bcb4f
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2025-02-04 23:11:37 -0600

    feat: rich text from contentful

--- src/Pages/ProjectView.jsx
+++ src/Pages/ProjectView.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
-
+import RichTextRendered from "../Components/RichTextRenderer";
+import { FaGithub } from "react-icons/fa";
 import client from "../client/contentful";
 import { useEffect, useState } from "react";
 import { RotateLoader } from "react-spinners";
@@ -44,28 +45,45 @@ const ProjectView = () => {
         )}
         {!loading && (
           <>
-            <h1 className="mx-auto text-white font-medium text-3xl">
-              {project && project.fields.projectTitle}
-            </h1>
+            <div className=" flex flex-row gap-1 justify-around items-center">
+              <h1 className="mx-auto text-white font-medium text-3xl">
+                {project && project.fields.projectTitle}
+              </h1>
+              {project && (
+                <a
+                  target="_blank"
+                  href={project.fields.gitHub}
+                  className=""
+                >
+                  <FaGithub
+                    size={36}
+                    className="text-white hover:scale-110 duration-300 transition"
+                  />
+                </a>
+              )}
+            </div>
 
             <label className="text-white my-3 italic text-center text-sm">
               {project && project.fields.description}
             </label>
+
             <ul className="flex flex-wrap gap-2 m-2">
-              {project &&  project.fields.tags.map((tag, index) => (
-                <li
-                  key={index}
-                  className="text-white border rounded-sm p-0.5 text-sm italic bg-slate-600 opacity-50 max-w-full truncate"
-                >
-                  {tag}
-                </li>
-              ))}
+              {project &&
+                project.fields.tags.map((tag, index) => (
+                  <li
+                    key={index}
+                    className="text-white border rounded-sm p-0.5 text-sm italic bg-slate-600 opacity-50 max-w-full truncate"
+                  >
+                    {tag}
+                  </li>
+                ))}
             </ul>
-           {
-             project &&  <a className="text-white italic text-sm font-light hover:font-normal transition duration-700" target="_blank"  href={project.fields.gitHub}>
-            Code to the repository
-          </a>
-           }
+
+            {project && (
+              <div className="text-white w-90 px-8 rounded-lg border border-white">
+                <RichTextRendered richText={project.fields.full} />
+              </div>
+            )}
           </>
         )}
       </div>
