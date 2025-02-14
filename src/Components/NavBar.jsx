5021905e4f2515fc9a0df7d1cc9821370a4e912a
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2025-02-13 22:33:09 -0600

    UI (NavBars): delete opacity

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-scroll";
 
 const NavBar = () => {
   return (
-    <div className=" fixed bottom-0 z-20 h-16 w-full flex justify-center opacity-75 items-center bg-slate-950">
+    <div className=" fixed bottom-0 z-20 h-16 w-full flex justify-center  items-center bg-slate-950">
       <ul className="text-white font-light flex flex-row gap-8">
         <Link to="Hero" smooth duration={500} className="hover:font-semibold hover:scale-105 cursor-pointer hover:text-blue-400 hover:mx-4 transition-all duration-750">
           Home
